b87865859d1b202e047be07303655057ddbd5b98
Alexandre Bertrand <alexandre.bertrand@invirtus.io>
2025-06-24 21:58:58 +0200

    fix feedback

--- src/Query/Mysql/Greatest.php
+++ src/Query/Mysql/Greatest.php
@@ -2,12 +2,14 @@
 
 namespace DoctrineExtensions\Query\Mysql;
 
+use Doctrine\ORM\Query\AST\ArithmeticExpression;
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
 use Doctrine\ORM\Query\TokenType;
 
-use function count;
+use function array_map;
+use function implode;
 
 /**
  * @author Vas N <phpvas@gmail.com>
@@ -15,46 +17,28 @@ use function count;
  */
 class Greatest extends FunctionNode
 {
-    private $field = null;
-
     private $values = [];
 
     public function parse(Parser $parser): void
     {
         $parser->match(TokenType::T_IDENTIFIER);
         $parser->match(TokenType::T_OPEN_PARENTHESIS);
-        $this->field = $parser->ArithmeticExpression();
-        $lexer       = $parser->getLexer();
+        $lexer = $parser->getLexer();
 
-        while (
-            count($this->values) < 1 ||
-            $lexer->lookahead->type !== TokenType::T_CLOSE_PARENTHESIS
-        ) {
+        do {
             $parser->match(TokenType::T_COMMA);
             $this->values[] = $parser->ArithmeticExpression();
-        }
+        } while ($lexer->lookahead->type !== TokenType::T_CLOSE_PARENTHESIS);
 
         $parser->match(TokenType::T_CLOSE_PARENTHESIS);
     }
 
     public function getSql(SqlWalker $sqlWalker): string
     {
-        $query = 'GREATEST(';
+        $values = array_map(static function (ArithmeticExpression $value) use ($sqlWalker): string {
+            return $value->dispatch($sqlWalker);
+        }, $this->values);
 
-        $query .= $this->field->dispatch($sqlWalker);
-
-        $query .= ', ';
-
-        for ($i = 0; $i < count($this->values); $i++) {
-            if ($i > 0) {
-                $query .= ', ';
-            }
-
-            $query .= $this->values[$i]->dispatch($sqlWalker);
-        }
-
-        $query .= ')';
-
-        return $query;
+        return 'GREATEST(' . implode(', ', $values) . ')';
     }
 }
